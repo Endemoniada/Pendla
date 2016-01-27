9665457ef4e9f06e5a8d08669203f4525af0ee71
Martin Jönsson <nathaniel@endemoniada.org>
2016-01-27 15:46:24 +0100

    lägger till instruktioner för konfiguration, snyggar till kommentarer

--- pendla.py
+++ pendla.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Lägg till modulen för pendeltåg
+# Lägg till moduler för pendeltåg och bussar
 import trains
-# Lägg till modulen för bussar
 import buses
 
-# trains.setStationId("9507")
-# trains.setWalkTime(16)
-# trains.setTimeWindow("60")
-
-# API-nyckel: 72e87e92af514d73830ba8cf89b8197d
 key = "72e87e92af514d73830ba8cf89b8197d"
 
+# Ändra dessa värden till de stations- och hållplatsID du vill se, samt önskat tidsfönster och tiden det tar dig att gå dit.
+# (key, "<ID för hållplats/station>", "<tidsfönster>", <gångavstånd i minuter>)
 # Exekvera pendeltågssökning
 trains.getTrains(key, "9507", "60", 16)
-
 # Exekvera buss-sökning
 buses.getBuses(key, "3747", "30", 5)
