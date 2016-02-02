d05ebf01f2c0b022cb95c9a944a6b6d20af8b204
Martin Jönsson <nathaniel@endemoniada.org>
2016-02-02 10:16:51 +0100

    Flyttat in mer kod så att output iaf funkar korrekt

--- main.py
+++ main.py
@@ -19,10 +19,36 @@ class Station(object):
     takes to walk to it.
     """
 
-    def pop_departures(self):
-        for x in self.api_data['ResponseData'][traffic_type] if api_data and traffic_type:
-            if x[traffic_type] == self.traffic_type:
-                if x['']
+
+    def print_departures(self):
+        i = 0
+        for d in self.api_data['ResponseData'][self.traffic_type]:
+            currEpochTime = int(time())
+            timeTableEpoch = int(mktime(datetime.strptime(d['TimeTabledDateTime'], "%Y-%m-%dT%H:%M:%S").timetuple()))
+            expectedEpoch = int(mktime(datetime.strptime(d['ExpectedDateTime'], "%Y-%m-%dT%H:%M:%S").timetuple()))
+            timeTablePretty = datetime.strftime(datetime.strptime(d['TimeTabledDateTime'], "%Y-%m-%dT%H:%M:%S"), "%H:%M")
+            ExpectedPretty = datetime.strftime(datetime.strptime(d['ExpectedDateTime'], "%Y-%m-%dT%H:%M:%S"), "%H:%M")
+
+            if i >= 2:
+                break
+
+            if (timeTableEpoch-currEpochTime) < self.distance * 60:
+                continue
+
+            # print color.YELLOW + '%-7s' % "X min" + color.END,
+            print color.END + getLeaveTime(self.distance * 60, currEpochTime, expectedEpoch),
+            print color.YELLOW + '%-7s' % d['DisplayTime'] + color.END,
+            if timeTableEpoch == expectedEpoch:
+                # Visa tabelltid
+                print '%-11s' % timeTablePretty,
+            # ...Annars printa både tabelltid samt ny tid
+            else:
+                # Visa tabelltid
+                print '%-11s' % (timeTablePretty+"/"+color.RED+ExpectedPretty+color.END),
+            print color.YELLOW + '%-11s' % d['Destination'] + color.END
+            if d['Deviations']:
+                print color.DARKCYAN + "- " + d['Deviations'][0]['Text'][:80] + color.END
+            i += 1
 
     def __init__(self):
         self.station_id = None
@@ -50,6 +76,26 @@ class color:
     END = '\033[0m'
 
 
+def getLeaveTime(wt, ct, dt):
+    # wt=walktime, ct=currtime, dt=departuretime
+
+    # 1. räkna ut sekunder till avgång
+    # 2. subtrahera tiden det tar att gå
+    # 3. avrunda uppåt till minuter
+    # 4. om minuter < 0, skicka "Nu!"
+
+    timeLeftS = (dt - ct) - wt
+    # print "timeLeftS="+str(timeLeftS)
+
+    timeLeftM = int(float(timeLeftS) / 60.0)
+    # print "TimeLeftM="+str(timeLeftM)
+
+    if timeLeftM <= 0:
+        return color.BOLD + color.RED + '%-7s' % "Nu!" + color.END
+    if timeLeftM <= 5:
+        return color.BOLD + color.YELLOW + '%-7s' % (str(timeLeftM)+" min") + color.END
+    return color.YELLOW + '%-7s' % (str(timeLeftM)+" min") + color.END
+
 def get_api_json_data(api_key, station_id):
     url = "http://api.sl.se/api2/realtimedepartures.json?key=" + \
             api_key + "&siteid=" + station_id + "&timewindow=60"
@@ -71,7 +117,7 @@ def print_header(traffic_type):
         print '%-11s' % "Tid",
         print "Linje",
         print "Destination"
-        print
+        print color.END
     elif traffic_type is "Trains":
         print color.DARKCYAN+color.BOLD + "PENDELTÅG" + color.END
         # Printa ut lite kolumner
@@ -79,7 +125,7 @@ def print_header(traffic_type):
         print '%-8s' % "Avgång",
         print '%-11s' % "Tid",
         print "Destination"
-        print
+        print color.END
 
 def remaining_time(distance, departure):
     """Take walking time (in minutes) and departure time (as string)
@@ -108,6 +154,7 @@ def main():
     helenelund.api_data = get_api_json_data(api_key, helenelund.station_id)
 
     print_header(helenelund.traffic_type)
+    helenelund.print_departures()
 
     print_header("bus")
 
