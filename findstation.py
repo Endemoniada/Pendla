ac9a0dd310d663eef2d222e629a71ed0c6f84cfe
Martin Jönsson <nathaniel@endemoniada.org>
2016-02-04 09:40:38 +0100

    findstation.py is functional. It can take input and display results

--- findstation.py
+++ findstation.py
@@ -11,12 +11,40 @@ try:
     from urllib.request import urlopen
 except ImportError:
     # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+    from urllib2 import urlopen, quote
 import json
 
 
+def get_search_string():
+    string = raw_input("Sök efter hållplats: ")
+    print
+    return string
+
+
 def main():
-    pass
+    API_KEY = "e24b05ad190347b3aa55284738027712"
+
+    # api.sl.se/api2/typeahead.json?key=e24b05ad190347b3aa55284738027712&searchstring=Helenelund&stationsonly=True&maxresults=10
+
+    search_string = get_search_string()
+    stations_only = "True"
+    max_results = "10"
+
+    url = "http://api.sl.se/api2/typeahead.json?key=" + API_KEY + \
+        "&searchstring=" + quote(search_string) + \
+        "&stationsonly=" + stations_only + \
+        "&maxresults=" + max_results
+    stream = urlopen(url)
+    data = json.load(stream)
+
+    if data['StatusCode'] != 0:
+        print "StatusCode: %s\nMessage: %s" % (
+            data['StatusCode'], data['Message']
+        )
+    else:
+        print "Resultat:"
+        for r in data['ResponseData']:
+            print r['SiteId'] + " " + r['Name']
 
 if __name__ == '__main__':
     main()
