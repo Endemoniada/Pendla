f9a5dfe11440c7480be1ff0433bd5b752f4c123c
Martin Jönsson <nathaniel@endemoniada.org>
2016-02-04 08:55:54 +0100

    Creates findstation.py

---
+++ findstation.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+# TO-DO
+#
+# - Take input for search
+# - Format search URL and fetch results
+# - Parse results and allow user to choose one
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+import json
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
